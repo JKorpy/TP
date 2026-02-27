5ba1c2c10935ea6108118ca429c0da5608eacb8d
nicoola91 <x24301680@student.ncirl.ie>
2026-02-27 11:02:28 +0000

    login functions done

--- app.js
+++ app.js
@@ -4,18 +4,28 @@
 
 //Import 
 const express = require("express");
+//importing the express module
+const session = require('express-session')
 //Express App
 const app = express();
 //View Engine
 app.set("view engine", "ejs");
+
 
 //Middleware for static files
 //Parse JSON data
 app.use(express.json());
 //Use static files in public folder
 app.use(express.static("public"));
-//Parse URL-encoded dta for form inputs
-app.use(express.urlencoded({ extended: true }));
+//default set to true so set to false to use library querystring
+app.use(express.urlencoded({ extended: false }));
+
+app.use(session({
+    secret: 'my_session_secret',
+    resave: true,
+    saveUninitialized: false
+}))
+
 
 app.listen(3000);
 
@@ -31,8 +41,19 @@ app.get("/catalogue", (request, response) => {
 
 //Login Page
 app.get("/login", (request, response) => {
-    response.render("login", {title: "Login"});
+    response.render("login", {title: "Login", error: null});
 });
+
+//login post route to recieve the username and password from form
+app.post('/Login', (req, res) => {
+    if  (req.body.username === 'john'&& req.body.password ==='123'){
+        //create session - i will replace with database
+        req.session.user = { id: 1, username: 'john', name: 'John Doe'}
+        res.redirect('/')
+    }else{
+        res.render('Login',{error : 'Wrong Credentils'})
+    }
+})
 
 //Register Page
 app.get("/register", (request, response) => {
