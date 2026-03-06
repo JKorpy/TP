af8124a6b2d428a32702061aac0e6f290c5dc906
Jason <x24302864@student.ncirl.ie>
2026-03-06 15:20:47 +0000

    modal in progress

--- db.js
+++ db.js
@@ -2,10 +2,16 @@
 // This is kept in a separate file so the same database connection can be reused
 // across the application without duplicating configuration code.
 require("dotenv").config();
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = { pool };
