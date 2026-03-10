35a202ce3b77e364b1c93806377cb8324a211712
nicoola91 <x24301680@student.ncirl.ie>
2026-03-10 09:14:53 +0000

    changed DB issues, added design, didnt change basket, strange

--- db.js
+++ db.js
@@ -2,10 +2,20 @@
 // This is kept in a separate file so the same database connection can be reused
 // across the application without duplicating configuration code.
 require("dotenv").config();
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = { pool };
